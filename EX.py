ac3d71e8b621fc5dfb9534f914ed8b22860742f3
Mr. Ex <103701389+EX-OFFICIAL@users.noreply.github.com>
2023-05-28 02:48:44 +0600

    Add files via upload

---
+++ EX.py
@@ -0,0 +1,14 @@
+import os, platform
+print('\033[38;5;76mCONGRATULATIONS YOUR DEVICE HAS BEEN SUPPORT THIS TOOLS')
+print('\033[38;5;119mEX IS REAL\n\033[1;92m I AM EX OF NATIONS')
+os.system("git pull")
+os.system("xdg-open https://www.facebook.com/EX.OFFICI4L")
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from sex import main
+    main()
